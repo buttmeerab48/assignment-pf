56ceefcea2000f4fdd5835f3b4a71253c0679cb4
buttmeerab48 <buttmeerab48@gmail.com>
2025-12-09 21:03:22 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+STATEMENT 1:
+Write a program to assigned values to three values at the time of declaration. Print the assigned values.
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  int Age = 20;
+  string Name = "Hassan";
+  char Grade = 'A';
+    cout<<"Name=" <<Name <<endl;
+    cout<<"Age=" <<Age <<endl;
+    cout<<"Grade=" <<Grade <<endl;
+     return 0;
+}
